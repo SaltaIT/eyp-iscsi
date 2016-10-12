5deca3b99e0e0a637e2f39cf5a7d25ce1dde1c56
Jordi Prats <jprats@systemadmin.es>
2016-10-12 10:56:11 +0200

    bugfix fact

--- lib/facter/eyp_iscsi_initiatorname.rb
+++ lib/facter/eyp_iscsi_initiatorname.rb
@@ -1,9 +1,11 @@
-initiatorname = Facter::Util::Resolution.exec('bash -c \'cat /etc/iscsi/initiatorname.iscsi  | cut -f 2 -d=\'').to_s
+if File.exist?('/etc/iscsi/initiatorname.iscsi')
+  initiatorname = Facter::Util::Resolution.exec('bash -c \'cat /etc/iscsi/initiatorname.iscsi  | cut -f 2 -d=\'').to_s
 
-unless initiatorname.nil? or initiatorname.empty?
-  Facter.add('eyp_iscsi_initiatorname') do
-      setcode do
-        initiatorname
-      end
+  unless initiatorname.nil? or initiatorname.empty?
+    Facter.add('eyp_iscsi_initiatorname') do
+        setcode do
+          initiatorname
+        end
+    end
   end
 end
