45fbdc3b2f9033b641864257909bceb456d4d99a
Jordi Prats <jprats@systemadmin.es>
2016-10-13 14:56:52 +0200

    exec session funcionant

--- manifests/session.pp
+++ manifests/session.pp
@@ -1,4 +1,4 @@
-define iscsi::target($targetname = $name) {
+define iscsi::session($ifaces, $portals, $target = $name) {
 
   Exec{
     path => '/usr/sbin:/usr/bin:/sbin:/bin',
@@ -9,9 +9,18 @@ define iscsi::target($targetname = $name) {
     fail('You must include the iscsi base class before using any iscsi defined resources')
   }
 
+  validate_array($ifaces)
+  validate_array($portals)
+
+  file { '/tmp/session':
+    ensure => 'present',
+    content => template("${module_name}/session/execsession.erb"),
+
+  }
+
   exec { "interface iscsi ${targetname}":
     command => "bash -c 'iscsiadm -m discovery -p ${targetname} -t st; iscsiadm -m node -L automatic'",
-    unless  => "bash -c 'iscsiadm -m node'",
+    unless  => "iscsiadm -m node",
     require => Class['iscsi::service'],
   }
 
