3bd766fe9be7c2501264c5db8dd97eb30218034f
Jordi Prats <jprats@systemadmin.es>
2016-10-13 15:31:03 +0200

    fora debug

--- manifests/session.pp
+++ manifests/session.pp
@@ -12,12 +12,6 @@ define iscsi::session($ifaces, $portals, $target = $name) {
   validate_array($ifaces)
   validate_array($portals)
 
-  file { '/tmp/session':
-    ensure => 'present',
-    content => template("${module_name}/session/execsession.erb"),
-
-  }
-
   exec { "interface iscsi ${target} ${ifaces} ${portals}":
     command => template("${module_name}/session/execsession.erb"),
     unless  => "iscsiadm -m node",
