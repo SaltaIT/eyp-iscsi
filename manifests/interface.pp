c52199167ca114d7cc064133f14710de423cf488
Jordi Prats <jprats@systemadmin.es>
2016-12-01 14:38:06 +0100

    afegides notes

--- manifests/interface.pp
+++ manifests/interface.pp
@@ -19,4 +19,24 @@ define iscsi::interface($interface = $name) {
     require => Class['iscsi::install'],
   }
 
+  #revisar:
+  # 
+  # iscsiadm --mode node --logoutall=all
+  # iscsiadm -m iface -I eth4 --op=delete
+  # iscsiadm -m iface -I eth5 --op=delete
+  # iscsiadm -m iface -I eth4 --op=new
+  # iscsiadm -m iface -I eth5 --op=new
+  # iscsiadm -m iface -I eth4 --op=update -n iface.hwaddress -v 00:21:f6:16:af:c5
+  # iscsiadm -m iface -I eth5 --op=update -n iface.hwaddress -v 00:21:f6:74:9a:b9
+  # iscsiadm -m iface -I eth4 --op=update -n iface.mtu -v 9000
+  # iscsiadm -m iface -I eth5 --op=update -n iface.mtu -v 9000
+  # iscsiadm -m iface -I eth4 --op=update -n iface.initiatorname -v iqn.1994-05.com.redhat:evl8800545
+  # iscsiadm -m iface -I eth5 --op=update -n iface.initiatorname -v iqn.1994-05.com.redhat:evl8800545
+  # iscsiadm -m iface -I eth4 --op=update -n iface.ipaddress -v 10.155.147.77
+  # iscsiadm -m iface -I eth --op=update -n iface.ipaddress -v 10.155.147.141
+  # iscsiadm -m discovery -t sendtargets -p 10.155.147.123 -p 10.155.147.124 -p 10.155.147.125 -p 10.155.147.126 -I eth4
+  # iscsiadm -m discovery -t sendtargets -p 10.155.147.187 -p 10.155.147.188 -p 10.155.147.189 -p 10.155.147.190 -I eth5
+  # iscsiadm -m node -l
+  # multipath -v2
+
 }
