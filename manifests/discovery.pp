d9813b1e96115f2ba24e59a16e33ead834011f55
Jordi Prats <jprats@systemadmin.es>
2016-12-07 18:44:03 +0100

    afegit todo

---
+++ manifests/discovery.pp
@@ -0,0 +1,2 @@
+# iscsiadm -m discovery -t sendtargets -p 10.155.196.187 -p 10.155.196.188 -p 10.155.196.189 -p 10.155.196.190 -I ens1f1
+# iscsiadm -m discovery -t sendtargets -p 10.155.196.249 -p 10.155.196.250 -p 10.155.196.251 -p 10.155.196.252 -I ens3f1
