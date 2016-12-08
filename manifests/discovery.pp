69c4920608c0c7c934733d489857bb5df8e727a7
Jordi Prats <jprats@systemadmin.es>
2016-12-08 09:15:43 +0100

    afegit discovery

--- manifests/discovery.pp
+++ manifests/discovery.pp
@@ -1,2 +1,29 @@
-# iscsiadm -m discovery -t sendtargets -p 10.155.196.187 -p 10.155.196.188 -p 10.155.196.189 -p 10.155.196.190 -I ens1f1
-# iscsiadm -m discovery -t sendtargets -p 10.155.196.249 -p 10.155.196.250 -p 10.155.196.251 -p 10.155.196.252 -I ens3f1
+# iscsiadm -m discovery -t sendtargets -p 1.2.3.4 -p 5.6.7.8 (...) -I eth0
+define iscsi::discovery(
+                          $portals,
+                          $iface=$name,
+                          $debug=false
+                        ) {
+
+  Exec{
+    path => '/usr/sbin:/usr/bin:/sbin:/bin',
+  }
+
+  validate_string($iface)
+  validate_array($portals)
+
+  if($debug)
+  {
+    file { "/tmp/discovery-${iface}":
+      ensure => 'present',
+      content => template("${module_name}/discovery.erb"),
+    }
+  }
+
+  exec { "interface iscsi ${iface} ${portals}":
+    command => template("${module_name}/discovery.erb"),
+    unless  => "iscsiadm -m session",
+    require => Class['iscsi::service'],
+  }
+
+}
