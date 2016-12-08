f4f339254d22abc4760b7f335dae08faddafb691
Jordi Prats <jprats@systemadmin.es>
2016-12-08 17:44:45 +0100

    afegit sysctl

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -40,6 +40,7 @@ RSpec.configure do |c|
       on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module', 'install', 'eyp-eyplib'), { :acceptable_exit_codes => [0,1] }
       on host, puppet('module', 'install', 'eyp-multipathd'), { :acceptable_exit_codes => [0,1] }
+      on host, puppet('module', 'install', 'eyp-sysctl'), { :acceptable_exit_codes => [0,1] }
     end
   end
 end
