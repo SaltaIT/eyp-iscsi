d87ffc9c39211500d982a9c0e0770db5aec990ad
Jordi Prats <jprats@systemadmin.es>
2016-12-08 13:51:01 +0100

    lint

--- manifests/session.pp
+++ manifests/session.pp
@@ -10,13 +10,13 @@ define iscsi::session($ifaces, $portals, $target = $name) {
   validate_array($portals)
 
   file { "/tmp/session-${ifaces}":
-    ensure => 'present',
+    ensure  => 'present',
     content => template("${module_name}/session/execsession.erb"),
   }
 
   exec { "interface iscsi ${target} ${ifaces} ${portals}":
     command => template("${module_name}/session/execsession.erb"),
-    unless  => "iscsiadm -m session",
+    unless  => 'iscsiadm -m session',
     require => Class['iscsi::service'],
   }
 
