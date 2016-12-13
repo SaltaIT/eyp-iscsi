5e4585bc47950106d01745b0beefc075e29f8963
Jordi Prats <jprats@systemadmin.es>
2016-12-13 12:39:00 +0100

    lint

--- manifests/session.pp
+++ manifests/session.pp
@@ -22,7 +22,7 @@ define iscsi::session(
   #TODO: fer per iface
   exec { "interface iscsi ${target} ${iface} ${portals}":
     command => template("${module_name}/session/execsession.erb"),
-    unless  => "iscsiadm -m session -P 1 | grep \"Iface Name\" | grep ${iface}"
+    unless  => "iscsiadm -m session -P 1 | grep \"Iface Name\" | grep ${iface}",
     require => [ Class['iscsi::service'], Iscsi::Interface[$iface] ],
   }
 
